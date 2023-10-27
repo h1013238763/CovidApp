8e50ee53104a8b1e3f2247e8a91ae071378420cb
oscar maldonado-garcia <45194139+oscarmmgg1234@users.noreply.github.com>
2023-10-26 18:45:21 -0700

    Update API.py

--- Website/API.py
+++ Website/API.py
@@ -2,19 +2,29 @@ from flask import Blueprint, jsonify, request, redirect
 
 API = Blueprint('API', __name__)
 
-@API.route('/api/test', methods=['GET'])
-def test():
-    return jsonify(message="Hello from API"), 200
-# For demonstration purposes, using hardcoded username and password
+
 VALID_USERNAME = 'admin'
 VALID_PASSWORD = 'password'
+
 
 @API.route('/api/login', methods=['POST'])
 def login():
     data = request.get_json()
     if data.get("username") == VALID_USERNAME and data.get("password") == VALID_PASSWORD:
         return jsonify(userAuthenticated="true"), 200
+
+@API.route('/api/create_account', methods=['POST'])
+def create_count():
+    data = request.get_json()
+    if data.get("username") == VALID_USERNAME and data.get("password") == VALID_PASSWORD:
+        return jsonify(userAuthenticated="true"), 200
+
         
+#create "crate account endpoint"
+#create "reset password" -> prototype 2
+
+
+
 
 
 if __name__ == '__main__':
