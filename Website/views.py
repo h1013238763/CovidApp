2a91a317c35acf690b2f21fff19801fd93cdceb1
1013238763@qq.com <1013238763@qq.com>
2023-11-04 21:38:29 -0700

    bugs 2

--- Website/views.py
+++ Website/views.py
@@ -6,15 +6,3 @@ views = Blueprint('views', __name__)
 @views.route('/')
 def home():
     return render_template('SignPage.html')
-
-@views.route('/survey')
-def survey():
-    return render_template('survey.html')
-
-@views.route('/about')
-def about():
-    return render_template('about us.html')
-
-@views.route('/symptoms')
-def symptoms():
-    return render_template('covid symptoms.html')
