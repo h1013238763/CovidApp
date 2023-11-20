7be1c15ab9ca453080f2b3ef82d1e3995b523ec9
1013238763@qq.com <1013238763@qq.com>
2023-11-19 17:40:45 -0800

    11/19 version

--- Website/API.py
+++ Website/API.py
@@ -1,39 +1,167 @@
 from flask import Blueprint, jsonify, request, redirect
+import pickle
+import os
+from . import db 
+from .Models import User, BlogPost, SecurityQuestions  # Import User model # Import db instance
+import random
 
 API = Blueprint('API', __name__)
 
+current_directory = os.path.dirname(os.path.abspath(__file__))
+assets_directory = os.path.join(current_directory, 'Assets/Model')
+imputer_file_path = os.path.join(assets_directory, 'imputer.pkl')
+model_file_path = os.path.join(assets_directory, 'model.pkl')
 
-VALID_USERNAME = 'admin'
-VALID_PASSWORD = 'password'
+with open(model_file_path, "rb") as model_file:
+    model = pickle.load(model_file)
+
+with open(imputer_file_path, "rb") as imputer_file:
+    imputer = pickle.load(imputer_file)
+
+
+def reformat_input(content):
+    feature_map = {
+        'SEX': {'male': 0, 'female': 1},
+        'DIABETES': {'yes': 1, 'no': 0},
+        'OBESITY': {'yes': 1, 'no': 0},
+        'ASTHMA': {'yes': 1, 'no': 0},
+        'TOBACCO': {'yes': 1, 'no': 0},
+        'PATIENT_TYPE': {'yes': 1, 'no': 0}
+    }
+    
+    reformatted_content = {}
+    for feature, mapping in feature_map.items():
+        value = content.get(feature, '').lower()
+        reformatted_content[feature] = mapping.get(value, None)
+        
+    reformatted_content['AGE'] = int(content.get('AGE', 0))
+
+    return reformatted_content
+
+
+
+@API.route("/api/predict", methods=["POST"])
+def predict():
+    try:
+        #example input {"SEX": "male", "AGE": 20, "DIABETES": "yes", "OBESITY": "yes", "ASTHMA": "yes", "TOBACCO": "yes", "PATIENT_TYPE": "no"}
+        # Parse input features from JSON
+        content = request.json
+        reformatted_content = reformat_input(content)
+        
+        feature_array = [
+            reformatted_content['SEX'],
+            reformatted_content['AGE'],
+            reformatted_content['DIABETES'],
+            reformatted_content['OBESITY'],
+            reformatted_content['ASTHMA'],
+            reformatted_content['TOBACCO'],
+            reformatted_content['PATIENT_TYPE']
+        ]
+        # Handle missing values using the imputer
+        feature_array = imputer.transform([feature_array])
+
+        # Make prediction
+        prediction = model.predict_proba(feature_array)
+        
+        # Return probability (the second class)
+        return jsonify({"probability": prediction[0][1]})
+
+    except Exception as e:
+        return jsonify({"error": str(e)})
 
 
 @API.route('/api/login', methods=['POST'])
 def login():
     data = request.get_json()
-    if data.get("username") == VALID_USERNAME and data.get("password") == VALID_PASSWORD:
-        return jsonify(userAuthenticated="true"), 200
+    # Query the database for the user
+    user = User.query.filter_by(username=data.get("username")).first()
+    if user:
+        if user.password == data.get("password"):
+            return jsonify(userAuthenticated="true"), 200
+    elif user.password != data.get("password"):
+        return jsonify(userAuthenticated="false", error_mes="password incorrect"), 200
+    else: 
+        return jsonify(userAuthenticated="false", error_mes="username not found || create account..."), 200
+        
 
 @API.route('/api/create_account', methods=['POST'])
-def create_count():
+def createAccount():
     data = request.get_json()
-<<<<<<< HEAD
-    if data.get("username") in in_memory_user_dictionary:
-        return jsonify(userAuthenticated="false", error_mes="user already exist...login."), 200
+    if User.query.filter_by(username=data.get('username')).first():
+        return jsonify(userAccountCreated="false", error_mes="user already exist...login."), 200
     else:
-        in_memory_user_dictionary[data.get("username")] = {'password': data.get("password"), 'email': data.get("email")}
-        print(in_memory_user_dictionary)
-=======
-    if data.get("username") == VALID_USERNAME and data.get("password") == VALID_PASSWORD:
->>>>>>> parent of 8732299 (Prototype 1 Complete and all function been tested)
-        return jsonify(userAuthenticated="true"), 200
+        new_user = User(id=random.randint(1000,1000000),username=data.get("username"), password=data.get("password"), email=data.get("email"))
+        new_security_q1 = SecurityQuestions(id=random.randint(1000,1000000), username=new_user.username, question=data.get("question1"), answer=data.get("answer1"), qnumber=1)
+        new_security_q2 = SecurityQuestions(id=random.randint(1000,1000000), username=new_user.username, question=data.get("question2"), answer=data.get("answer2"), qnumber=2)
+        db.session.add(new_user)
+        db.session.add(new_security_q1)
+        db.session.add(new_security_q2)
+        db.session.commit()  # For debugging
+        for a in SecurityQuestions.query.all():
+            print(a.username, a.question, a.answer, a.qnumber, a.id)
+        return jsonify(userAccountCreated="true"), 200
 
-        
-#create "crate account endpoint"
-#create "reset password" -> prototype 2
+
+def to_dict(row):
+    return {column.name: getattr(row, column.name) for column in row.__table__.columns}
 
 
+#first step and window allowing user to enter username example input {"username": "test"}
+@API.route('/api/forgot_password', methods=['POST'])
+def forgotPassword():
+    data = request.get_json()
+    user = User.query.filter_by(username=data.get("username")).first()
+    if not user:
+        return jsonify(nextStep="false", error_mes="username not found || create account..."), 200
+    else:
+        security_q = SecurityQuestions.query.filter_by(username=data.get("username")).all()
+        questions = [to_dict(q) for q in security_q]
+        return jsonify(nextStep="true", security_questions=questions ), 200 
+#===================================================================================================
+
+
+#example input {"username": "test", "firstA": "test", "secondA": "test", "new_password": "test"}
+@API.route('/api/change_password', methods=['POST'])
+def verifySecurityQuestions():
+    data = request.get_json()
+    user = User.query.filter_by(username=data.get("username")).first()
+    if not user:
+        return jsonify(nextStep="false", error_mes="username not found || create account..."), 200
+    else:
+        security_q1 = SecurityQuestions.query.filter_by(username=user.username, answer=data.get("firstA")).first()
+        security_q2 = SecurityQuestions.query.filter_by(username=user.username, answer=data.get("secondA")).first()
+        # if not security_q1 or not security_q2:
+        #     return jsonify(nextStep="false", error_mes="security questionsincorrect..."), 200
+        if not security_q1:
+            return jsonify(nextStep="false", error_mes="first security question incorrect...", qindex=1), 200
+        if not security_q2:
+            return jsonify(nextStep="false", error_mes="second security question incorrect...", qindex=2), 200
+        if data.get("new_password") != "":
+            user.password = data.get("new_password")
+            db.session.commit()
+            return jsonify(nextStep="true", mess="password updated."), 200
+        else:
+            return jsonify(nextStep="false", err_mes="new username cannot be empty"), 200
+
+#example input {"username": "test12", title: "test", body: "test"}
+@API.route('/api/add_request_blog', methods=['POST'])
+def addRequestBlog():
+    data=request.get_json()
+    if data.get("title") == "" or data.get("body") == "":
+        return jsonify(blogPostRequested="false", error_mes="title and body cannot be empty"), 200
+    new_blog_post = BlogPost(id=random.randint(1000,1000000), title=data.get("title"), body=data.get("body"), author=data.get("username", verified=False))
+    db.session.add(new_blog_post)
+    db.session.commit()
+    return jsonify(blogPostRequested="true"), 200
+
+#just call the function to retrive user verified blog posts by us
+@API.route('/api/get_blog_posts', methods=['GET'])
+def getBlogPosts():
+    blog_posts = BlogPost.query.filter_by(verified=True).all()
+    posts = [to_dict(post) for post in blog_posts]
+    return jsonify(blog_posts=posts), 200
 
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    API.run(debug=True)
--- Website/static/Scripts/SignPageHandler.js
+++ Website/static/Scripts/SignPageHandler.js
@@ -1,13 +1,12 @@
 index = 1;
-const panel_list = [
-  "sign_in_panel",
-  "register_panel",
-  "forget_password_panel",
-  "reset_password_panel",
-];
+const panel_list = ["sign_in_panel", "register_panel", "forget_panel", "reset_panel"];
+user = "";
+quest1 = "";
+quest2 = "";
 
 // Sign Page Image Change Functions
-function ChangeImage() {
+function ChangeImage()
+{
   var div = document.getElementById("image_button_panel");
   var baseUrl = div.getAttribute("data-url");
   var image = document.getElementById("show_image");
@@ -18,45 +17,427 @@ function ChangeImage() {
     image.style.opacity = 1;
   }, 800);
 
-  var button = document.getElementById("image_button_" + index);
-  button.style.width = 45;
+    var button = document.getElementById("image_button_"+index);
+    button.style.width = 45;
 
-  other_index = index;
-  for (i = 0; i < 2; i++) {
-    other_index++;
-    if (other_index > 3) {
-      other_index = 1;
+    other_index = index
+    for(i = 0; i < 2; i ++)
+    {
+        other_index ++;
+        if(other_index > 3)
+        {
+            other_index =1
+        }
+        document.getElementById("image_button_"+other_index).style.width = 20;
     }
-    document.getElementById("image_button_" + other_index).style.width = 20;
-  }
 }
-function ChangeImageIndex(image_index) {
-  index = image_index;
-  ChangeImage();
+function ChangeImageIndex(image_index)
+{
+    index = image_index;
+    ChangeImage();
 }
-function ChangeIndex() {
-  index++;
-  if (index > 3) {
-    index = 1;
-  }
-  ChangeImage();
+function ChangeIndex()
+{
+    index++;
+    if(index > 3)
+    {
+        index = 1;
+    }
+    ChangeImage();
 }
 var image_change_interval = setInterval(ChangeIndex, 10000);
 
 // Panel Switch Functions
-function SwitchPanel(panel_index) {
-  for (i = 0; i < panel_list.length; i++) {
-    if (i == panel_index) {
-      console.log(panel_list[i] + " active");
-      document.getElementById(panel_list[i]).style.display = "grid";
-    } else {
-      console.log(panel_list[i] + " deactive");
-      var component = document.getElementById(panel_list[panel_index]);
-      console.log(component);
-      document.getElementById(panel_list[i]).style.display = "none";
+function SwitchPanel(panel_index)
+{  
+    for(i = 0; i < panel_list.length; i ++)
+    {
+        if(i == panel_index)
+        {
+            document.getElementById(panel_list[i]).style.display='grid';
+        }
+        else
+        {
+            document.getElementById(panel_list[i]).style.display='none';
+        }
     }
-  }
 }
 
-// Sign in
-function Signin() {}
+
+// Sign in Button event
+function SigninEvent()
+{
+    console.log("sign in 1");
+
+    const data = {
+        username: document.getElementById("sign_username_input").value,
+        password: document.getElementById("sign_password_input").value
+    };
+
+    // input content handler
+    var is_empty = false;
+    // console.log("username["+document.getElementById("sign_username_input").value+"]");
+    if(data.username == "")
+    {
+        document.getElementById("sign_username_info").textContent = "please enter username";
+        is_empty = true;
+    }
+    else
+    {
+        document.getElementById("sign_username_info").textContent = "";
+    }
+    // console.log("password["+document.getElementById("sign_password_input").value+"]");
+    if(data.password == "")
+    {
+        document.getElementById("sign_password_info").textContent = "please enter password";
+        is_empty = true;
+    }
+    else
+    {
+        document.getElementById("sign_password_info").textContent = "";
+    }
+    if(is_empty)
+    {
+        return;
+    }
+
+    fetch("http://127.0.0.1:5000/api/login", {
+        method: "POST",
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(data),
+        redirect : "follow"
+    })
+    .then(response => response.json())
+    .then(result => {
+        if(result.userAuthenticated)
+        {
+            console.log("true");
+            // transfer to user page
+            window.location.href = '/survey';
+        } 
+        else
+        {
+            console.log("true");
+            document.getElementById("sign_password_info").textContent = "wrong password";
+        }  
+    })
+    .catch(error => {
+        console.error('Error:', error);
+    });
+}
+
+// Create Account Button event
+function CreateAccountEvent()
+{
+    console.log("create 1");
+    // input content handler
+    var is_illegal = false;
+    var is_empty = false;
+    
+    const data = {
+        username: document.getElementById("register_username_input").value,
+        password: document.getElementById("register_password_input").value,
+        email: document.getElementById("register_email_input").value,
+        question1: document.getElementById("register_question_1_input").value,
+        answer1: document.getElementById("register_answer_1_input").value,
+        question2: document.getElementById("register_question_2_input").value,
+        answer2: document.getElementById("register_answer_2_input").value,
+    };
+    // username check
+    if(data.username.length < 6 || data.username.length > 12)
+    {
+        document.getElementById("register_username_info").style.color = "#ff6565";
+        is_illegal = true;
+    }
+    else
+    {
+        document.getElementById("register_username_info").style.color = "gray";
+    }
+    // email check
+    if(data.email.indexOf('@') !== -1 && data.email.indexOf('.') !== -1)
+    {
+        if( data.email.indexOf('@') >= data.email.indexOf('.'))
+        {
+            document.getElementById("register_email_info").textContent = "please enter vaild email";
+            is_illegal = true;
+        }
+        else
+        {
+            document.getElementById("register_email_info").textContent = "";
+        }
+    }
+    else
+    {
+        document.getElementById("register_email_info").textContent = "please enter vaild email";
+        is_illegal = true;
+    }
+    // password check
+    if(data.password.length < 6 || data.password.length > 12)
+    {
+        document.getElementById("register_password_info").style.color = "#ff6565";
+        is_illegal = true;
+    }
+    else
+    {
+        document.getElementById("register_password_info").style.color = "gray";
+    }
+    // password confirm check
+    if(document.getElementById("register_confirm_input").value !== data.password)
+    {
+        document.getElementById("register_confirm_info").textContent = "password confirmation is incorrect";
+        is_illegal = true;
+    }
+    else
+    {
+        document.getElementById("register_confirm_info").textContent = "";
+    }
+
+    if(data.username == "")
+    {
+        is_empty = true;
+        document.getElementById("register_username_info").textContent = "Please enter content";
+    }
+    else
+    {
+        document.getElementById("register_username_info").textContent = "";
+    }
+    if(data.password == "")
+    {
+        is_empty = true;
+        document.getElementById("register_password_info").textContent = "Please enter content";
+    }
+    else
+    {
+        document.getElementById("register_password_info").textContent = "";
+    }
+    if(data.email == "")
+    {
+        is_empty = true;
+        document.getElementById("register_email_info").textContent = "Please enter content";
+    }
+    else
+    {
+        document.getElementById("register_email_info").textContent = "";
+    }
+    if(document.getElementById("register_confirm_input").value == "")
+    {
+        is_empty = true;
+        document.getElementById("register_confirm_info").textContent = "Please enter content";
+    }
+    else
+    {
+        document.getElementById("register_confirm_info").textContent = "";
+    }
+    if(data.question1 == "")
+    {
+        is_empty = true;
+        document.getElementById("register_question_1_info").textContent = "Please enter content";
+    }
+    else
+    {
+        document.getElementById("register_question_1_info").textContent = "";
+    }
+    if(data.answer1 == "")
+    {
+        is_empty = true;
+        document.getElementById("register_answer_1_info").textContent = "Please enter content";
+    }
+    else
+    {
+        document.getElementById("register_answer_1_info").textContent = "";
+    }
+    if(data.question2 == "")
+    {
+        is_empty = true;
+        document.getElementById("register_question_2_info").textContent = "Please enter content";
+    }
+    else
+    {
+        document.getElementById("register_question_2_info").textContent = "";
+    }
+    if(data.answer2 == "")
+    {
+        is_empty = true;
+        document.getElementById("register_answer_2_info").textContent = "Please enter content";
+    }
+    else
+    {
+        document.getElementById("register_answer_2_info").textContent = "";
+    }
+
+    if(is_illegal || is_empty)
+    {
+        return;
+    }
+
+    console.log("create 2");
+    // transfer data
+
+    fetch("http://127.0.0.1:5000/api/create_account", {
+        method: "POST",
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(data),
+        redirect : "follow"
+    })
+    .then(response => response.json())
+    .then(result => {
+        if(result.userAuthenticated)
+        {
+            console.log("true");
+            SwitchPanel(0);
+        } 
+        else
+        {
+            console.log(data);
+            console.log(result);
+            document.getElementById("register_email_info").textContent = "email or username is already in use";
+        }  
+    })
+    .catch(error => {
+        console.error('Error:', error);
+    });
+}
+
+function ForgetEvent()
+{
+    const data = {
+        username: document.getElementById("forget_username_input").value
+    };
+
+    if(data.username == "")
+    {
+        document.getElementById("forget_username_info").textContent = "Please enter your username";
+        return;
+    }
+
+    fetch("http://127.0.0.1:5000/api/forgot_password", {
+        method: "POST",
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(data),
+        redirect : "follow"
+    })
+    .then(response => response.json())
+    .then(result => {
+        if(result.userAuthenticated)
+        {
+            console.log(result);
+            // transfer to user page
+            SwitchPanel(3);
+        } 
+        else
+        {
+            console.log("false");
+            document.getElementById("forget_username_info").textContent = "username not exist";
+        }  
+    })
+    .catch(error => {
+        console.error('Error:', error);
+    });
+}
+
+function ResetEvent()
+{
+    const data = {
+        answer:document.getElementById("reset_code_input").value,
+        username:document.getElementById("forget_email_input").value,
+        password: document.getElementById("reset_password_input").value
+    };
+
+    var is_empty = false;
+    var is_illegal = false;
+
+    // username check
+    if(data.username.length < 6 || data.username.length > 12)
+    {
+        document.getElementById("reset_username_info").style.color = "#ff6565";
+        is_illegal = true;
+    }
+    else
+    {
+        document.getElementById("reset_username_info").style.color = "gray";
+    }
+    // password check
+    if(data.password.length < 6 || data.password.length > 12)
+    {
+        document.getElementById("reset_password_info").style.color = "#ff6565";
+        is_illegal = true;
+    }
+    else
+    {
+        document.getElementById("reset_password_info").style.color = "gray";
+    }
+    // password confirm check
+    if(document.getElementById("reset_confirm_input").value !== data.password)
+    {
+        document.getElementById("reset_confirm_info").textContent = "password confirmation is incorrect";
+        is_illegal = true;
+    }
+    else
+    {
+        document.getElementById("reset_confirm_info").textContent = "";
+    }
+
+    if(username == "")
+    {
+        is_empty = true;
+        document.getElementById("reset_username_info").textContent = "Please enter content";
+    }
+    else
+    {
+        document.getElementById("reset_username_info").textContent = "";
+    }
+    if(password == "")
+    {
+        is_empty = true;
+        document.getElementById("reset_password_info").textContent = "Please enter content";
+    }
+    else
+    {
+        document.getElementById("reset_password_info").textContent = "";
+    }
+    if(securityAnswer == "")
+    {
+        is_empty = true;
+        document.getElementById("reset_answer_info").textContent = "Please enter content";
+    }
+    else
+    {
+        document.getElementById("reset_answer_info").textContent = "";
+    }
+
+    if(is_illegal || is_empty)
+    {
+        return;
+    }
+
+    fetch("http://127.0.0.1:5000/api/create_account", {
+        method: "POST",
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(data),
+        redirect : "follow"
+    })
+    .then(response => response.json())
+    .then(result => {
+        if(result.userAuthenticated)
+        {
+            console.log(result);
+            SwitchPanel(0);
+        } 
+        else
+        {
+            console.log("true");
+            document.getElementById("reset_answer_info").textContent = "security answer wrong";
+        }  
+    })
+    .catch(error => {
+        console.error('Error:', error);
+    });
+}
